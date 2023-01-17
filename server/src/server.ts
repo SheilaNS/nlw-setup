433eaf3f1c03c89fed890c0af40de3dd4d0b9f54
SheilaNS <shei.nsantos@gmail.com>
2023-01-16 22:16:11 -0300

    feat(prisma): query test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient(); // faz a conexão com todas as tabelas do bd
 
-app.get('/', () => {
-  return 'Hello world!';
+app.get('/bd-test', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber'
+      }
+    }
+  })
+  return habits;
 })
 
 app.listen({
