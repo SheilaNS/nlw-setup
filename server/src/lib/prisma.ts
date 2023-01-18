0c516dd9382e0090e18f44344cadf608eef105d4
SheilaNS <shei.nsantos@gmail.com>
2023-01-18 17:18:41 -0300

    feat(server): get day route done

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient(); // faz a conexão com todas as tabelas do bd
+export const prisma = new PrismaClient({
+  log: ['query'],
+}); // faz a conexão com todas as tabelas do bd
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,7 @@ export async function appRoutes(app: FastifyInstance) {
     const creatHabitBody = z.object({
       title: z.string(),
       weekDays: z.array(z.number().min(0).max(6)),
-    })
+    });
 
     const { title, weekDays} = creatHabitBody.parse(request.body);
 
@@ -27,8 +27,47 @@ export async function appRoutes(app: FastifyInstance) {
           })
         }
       }
-    })
-    return ;
-  })
+    });
+  });
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date() // transforma uma string em data
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parseDate = dayjs(date).startOf('day');
+    const weekDay = parseDate.get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      }
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      }
+    });
+
+    const completeedHabits = day?.dayHabits.map((dayHabit) => dayHabit.id);
+
+    return {
+      possibleHabits,
+      completeedHabits
+    };
+  });
 }
 
