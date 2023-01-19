7f12891f3657252c1cffff1fb9213c39dd5d02a1
SheilaNS <shei.nsantos@gmail.com>
2023-01-19 17:16:37 -0300

    feat(mobile): add touchableProps

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,6 @@
-import { Dimensions, TouchableOpacity } from "react-native";
+import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+interface HabitDayProps extends TouchableOpacityProps {}
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -7,12 +9,13 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   Dimensions.get("screen").width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
 
-export function HabitDay() {
+export function HabitDay({ ...rest }: HabitDayProps) {
   return (
     <TouchableOpacity
       className="bg-zinc-900 border-2 border-zinc-800 rounded-lg m-1"
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.6}
+      {...rest}
     />
   );
 }
