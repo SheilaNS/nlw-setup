b41a0a3591db91cab19c211682b5046385584771
SheilaNS <shei.nsantos@gmail.com>
2023-01-19 15:24:55 -0300

    feat(mobile): Habit and New.tsx creeated

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export function Habit() {
+  return (
+    <View className="flex-1 bg-background pt-16 px-8">
+      
+    </View>
+  );
+}
---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export function New() {
+  return (
+    <View className="flex-1 bg-background pt-16 px-8">
+      
+    </View>
+  );
+}
