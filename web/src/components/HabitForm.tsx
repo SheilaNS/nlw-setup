9a44160d1b4b99fce9050b0af2bf2ed849603434
SheilaNS <shei.nsantos@gmail.com>
2023-01-21 19:20:25 -0300

    feat(web): focus style add

--- web/src/components/HabitForm.tsx
+++ web/src/components/HabitForm.tsx
@@ -53,7 +53,7 @@ export function HabitForm() {
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
         autoFocus
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-900 focus:ring-offset-2 focus:ring-offset-zinc-900"
         onChange={(event) => setTitle(event.target.value)}
         value={title}
       />
@@ -64,12 +64,12 @@ export function HabitForm() {
       <div className="mt-3 flex flex-col gap-2">
         {availableDays.map((day, i) => (
           <Checkbox.Root
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none"
             key={`${day}-${i}`}
             onCheckedChange={() => handleDayCheck(i)}
             checked={weekDays.includes(i)}
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-900 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -82,7 +82,7 @@ export function HabitForm() {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500 transition-colors"
+        className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-900 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
--- web/src/components/HabitList.tsx
+++ web/src/components/HabitList.tsx
@@ -56,13 +56,13 @@ export function HabitList({ date, completedToggle }: HabitListProps) {
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
             key={habit.id}
             disabled={pastDay}
             checked={habitsInfo.completedHabits.includes(habit.id)}
             onCheckedChange={() => handleToggle(habit.id)}
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-900 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
